8b0f393018de2056fdcbc4a36d5b5a29c78f170e
Jose <josenattunilam@gmail.com>
2017-11-14 17:38:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jose/jerinproject/ScrollingActivity.java
@@ -0,0 +1,35 @@
+package com.example.jose.jerinproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        TabLayout tablayout = (TabLayout) findViewById(R.id.tab_layout);
+        tablayout.addTab(tablayout.newTab().setText("tab1"));
+        tablayout.addTab(tablayout.newTab().setText("tab2"));
+        tablayout.addTab(tablayout.newTab().setText("tab3"));
+
+    }
+}
